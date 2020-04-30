a7157d96f8a42df4e6976716a222e903e07d215f
sonofab1rd <westernreserve@gmail.com>
2020-04-29 20:26:04 -0400

    added transition to the logo

--- web/src/Layout/Header/Header.js
+++ web/src/Layout/Header/Header.js
@@ -1,13 +1,24 @@
+import useWindowScrollPosition from "@rehooks/window-scroll-position";
 import { Link } from "gatsby";
 import React from "react";
 import logo from "../../logo.png";
 import styles from "./Header.module.css";
 
 const Header = () => {
+  const maxWidth = 128 + 72;
+  const minWidth = 165;
+  let position = useWindowScrollPosition();
+  const logoWidth = maxWidth - position.y * 0.1;
+
   return (
     <header className={styles.Header}>
       <nav className={styles.Nav}>
-        <img className={styles.Logo} src={logo} alt="Bluebird Logo"></img>
+        <div
+          className={styles.LogoWrapper}
+          style={{ width: `${logoWidth > minWidth ? logoWidth : minWidth}px` }}
+        >
+          <img className={styles.Logo} src={logo} alt="Bluebird Logo" />
+        </div>
         <div className={styles.Main}>
           <Link className={styles.NavLink} to="/">
             Home
