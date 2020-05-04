4ad24ba365d6c93532572fcb9b522d110ede981f
Christopher Dierkens <cjdierkens@gmail.com>
2020-05-03 20:41:37 -0600

    Adding design services list placeholder and design process

--- web/src/pages/services.js
+++ web/src/pages/services.js
@@ -1,54 +1,71 @@
 import React from "react";
+import Container from "../components/container";
 import Layout from "../Layout";
 
 const ServicesPage = () => (
   <Layout title="Services">
-    <div className="px-4 py-24 max-w-screen-lg m-auto">
-      <p>
-        Spicy jalapeno bacon ipsum dolor amet pork chop turducken short ribs
-        salami cow prosciutto pork meatball pancetta leberkas sausage. Shoulder
-        fatback turkey, tri-tip kevin meatloaf chislic ground round pastrami.
-        Ball tip alcatra tenderloin, tongue ground round venison sirloin
-        pancetta hamburger pastrami prosciutto. Ham pig flank pastrami ham hock
-        frankfurter turkey. Sausage cupim fatback filet mignon, salami pork loin
-        pork drumstick leberkas shank chuck cow.
-      </p>
+    <Container className="py-24 grid grid-cols-8">
+      <figure className="col-span-3 col-start-2">
+        <img src="https://via.placeholder.com/458" alt="Placeholder" />
 
-      <p>
-        Meatloaf pancetta turducken shoulder turkey jowl ham boudin strip steak.
-        Brisket sausage cow pork chop short ribs short loin, bacon kielbasa.
-        Sirloin turducken jerky jowl, shankle spare ribs doner rump drumstick
-        pork chop cow sausage. Jowl pig kielbasa corned beef sausage jerky
-        prosciutto.
-      </p>
+        <figcaption>3D Drafting Example</figcaption>
+      </figure>
 
-      <p>
-        Ham hock boudin drumstick tongue short ribs pork loin. Jowl frankfurter
-        ball tip capicola tail spare ribs jerky chuck pork. Pancetta beef bacon
-        jowl cow rump filet mignon swine porchetta kevin tongue. Shank flank
-        cupim tenderloin shoulder brisket ribeye salami corned beef chicken beef
-        ribs. Burgdoggen cupim brisket bacon beef ribeye strip steak drumstick
-        ball tip cow chislic tenderloin. Boudin spare ribs flank bresaola
-        hamburger ribeye pork shank. T-bone pork tenderloin ribeye tail salami.
-      </p>
+      <div className="col-span-3">
+        <ul className="list-disc pl-10">
+          <li>Cabinetry Design &amp; Sales </li>
+          <li>
+            Remodeling
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
+              dictum vel libero ac mollis. Fusce sollicitudin finibus leo quis
+              faucibus. Nullam porttitor sit amet ex.
+            </p>
+          </li>
+          <li>
+            3D Drafting
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
+              dictum vel libero ac mollis. Fusce sollicitudin finibus leo quis
+              faucibus. Nullam porttitor sit amet ex.
+            </p>
+          </li>
+          <li>Space Planning and Layout</li>
+          <li>
+            Selections
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
+              dictum vel libero ac mollis. Fusce sollicitudin finibus leo quis
+              faucibus. Nullam porttitor sit amet ex.
+            </p>
+          </li>
+        </ul>
+      </div>
+    </Container>
 
-      <p>
-        Spare ribs burgdoggen cow porchetta, shoulder jowl biltong brisket pork
-        tail landjaeger beef filet mignon. Fatback cow flank, bresaola filet
-        mignon leberkas bacon. Drumstick pork capicola tail flank. Strip steak
-        pig venison jerky kevin andouille frankfurter pancetta t-bone tri-tip
-        kielbasa short ribs. Alcatra flank burgdoggen, strip steak short ribs
-        salami andouille chuck.
-      </p>
+    <Container className="grid grid-cols-8">
+      <h2 className="text-center col-span-8 text-blue-dark">Design Process</h2>
 
-      <p>
-        Sausage flank kielbasa hamburger beef beef ribs venison capicola
-        landjaeger biltong. Kielbasa landjaeger shoulder cupim shank chuck.
-        Andouille kevin chicken chuck. Pork loin chuck buffalo, pancetta doner
-        strip steak rump sirloin pork belly. Chicken pork capicola beef ribs,
-        cupim salami drumstick buffalo biltong doner turkey.
-      </p>
-    </div>
+      <div className="bg-gray-light col-span-4">
+        <h3>Initial Meeting</h3>
+
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dictum
+          vel libero ac mollis. Fusce sollicitudin finibus leo quis faucibus.
+          Nullam porttitor sit amet ex.
+        </p>
+      </div>
+
+      <div className="bg-gray-light col-span-4">
+        <h3>Initial Meeting</h3>
+
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi dictum
+          vel libero ac mollis. Fusce sollicitudin finibus leo quis faucibus.
+          Nullam porttitor sit amet ex.
+        </p>
+      </div>
+    </Container>
   </Layout>
 );
 
