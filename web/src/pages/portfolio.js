a766e3ac75487dc0225f29378f3e39bb167de3d0
Christopher Dierkens <cjdierkens@gmail.com>
2020-05-19 23:05:17 -0600

    Initial pass at filterin portfolio images

--- web/src/pages/portfolio.js
+++ web/src/pages/portfolio.js
@@ -1,7 +1,8 @@
-import { graphql } from "gatsby";
+import { graphql, navigate } from "gatsby";
 import PropTypes from "prop-types";
 import React, { useState } from "react";
 import Carousel, { Modal, ModalGateway } from "react-images";
+import useQueryString from "use-query-string";
 import Button from "../components/Button";
 import Container from "../components/container";
 import KindWords from "../components/KindWords";
@@ -32,13 +33,35 @@ const PortfolioPage = ({
     images: { nodes: portfolioImages },
   },
   errors,
+  location,
 }) => {
   const [selectedIndex, setSelectedIndex] = useState(0);
   const [lightboxIsOpen, setLightboxIsOpen] = useState(false);
 
-  const images = portfolioImages.map(({ image }) => ({
-    src: builder.image(image.file.asset.id).height(768).fit("clip").url(),
-  }));
+  const [query, setQuery] = useQueryString(location, (value) =>
+    navigate(value, { replace: true })
+  );
+
+  const tags = Array.from(
+    portfolioImages.reduce(
+      (keys, node) => new Set([...node.tags, ...keys]),
+      new Set()
+    )
+  );
+
+  const checkedTags = Object.entries(query)
+    .filter(([_, value]) => value === "true")
+    .map(([key]) => key);
+
+  const images = portfolioImages
+    .filter(({ tags }) =>
+      tags.find((tag) =>
+        checkedTags.find((checkedTag) => checkedTag === encodeURI(tag))
+      )
+    )
+    .map(({ image }) => ({
+      src: builder.image(image.file.asset.id).height(768).fit("clip").url(),
+    }));
 
   const openLightbox = (index) => {
     setSelectedIndex(index);
@@ -57,22 +80,55 @@ const PortfolioPage = ({
         </Select>
       </Formik> */}
 
+        <div>
+          {tags.map((tag) => {
+            return (
+              <div className="inline-block">
+                <label className="flex items-center cursor-pointer">
+                  <span className="relative h-4 w-4">
+                    <input
+                      className="absolute z-0 w-full h-full focus:outline-none focus:shadow-outline shadow-md"
+                      type="checkbox"
+                      checked={query[encodeURI(tag)] === "true"}
+                      onChange={({ target: { checked } }) =>
+                        setQuery({ [encodeURI(tag)]: String(checked) })
+                      }
+                    />
+                  </span>
+                  <span className="font-sans text-sm ml-2">{tag}</span>
+                </label>
+              </div>
+            );
+          })}
+        </div>
+
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3 pt-6">
-          {portfolioImages.map(({ image }, index) => (
-            <div key={image.file.asset.id}>
-              <button onClick={() => openLightbox(index)}>
-                <img
-                  className="transform hover:scale-105 duration-300 p-1 shadow-md"
-                  src={builder
-                    .image(image.file.asset.id)
-                    .size(408, 272)
-                    .fit("crop")
-                    .url()}
-                  alt={image.description}
-                />
-              </button>
-            </div>
-          ))}
+          {portfolioImages
+            .filter(({ tags }) =>
+              tags.find((tag) =>
+                checkedTags.find((checkedTag) => checkedTag === encodeURI(tag))
+              )
+            )
+            .map(({ image, tags }, index) => (
+              <div key={image.file.asset.id}>
+                <button onClick={() => openLightbox(index)}>
+                  <img
+                    className="transform hover:scale-105 duration-300 p-1 shadow-md"
+                    src={builder
+                      .image(image.file.asset.id)
+                      .size(408, 272)
+                      .fit("crop")
+                      .url()}
+                    alt={image.description}
+                  />
+                </button>
+                {tags.map((tag) => (
+                  <span className="py-1 px-2 m-1 bg-white rounded shadow-md">
+                    {tag}
+                  </span>
+                ))}
+              </div>
+            ))}
 
           {/* <div>
           <span className="bg-gray-light rounded-full text-center font-medium p-1 inline-block w-8 h-8 mr-3">
@@ -119,6 +175,7 @@ PortfolioPage.propTypes = {
       message: PropTypes.string,
     })
   ),
+  location: PropTypes.object.isRequired,
 };
 
 export default PortfolioPage;
