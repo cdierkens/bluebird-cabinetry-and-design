d112a159a16d69377660b70dd75b6ffd4fe43946
Christopher Dierkens <christopher.dierkens@formidable.com>
2021-04-11 15:30:40 -0400

    container -> Container

--- web/src/components/Container/Container.tsx
+++ web/src/components/Container/Container.tsx
@@ -1,6 +1,6 @@
 import clsx from "clsx";
 import React from "react";
-import * as styles from "./container.module.css";
+import * as styles from "./Container.module.css";
 
 interface ContainerProps {
   className?: string;
