faba097c0256dfd59bf166b9d069faf06826bd99
Christopher Dierkens <christopher.dierkens@formidable.com>
2021-07-17 11:06:19 -0400

    make theme properties required

--- web/src/components/PortfolioImages/constants.ts
+++ web/src/components/PortfolioImages/constants.ts
@@ -1,10 +1,1 @@
-import resolveConfig from "tailwindcss/resolveConfig";
-import { TailwindConfig } from "tailwindcss/tailwind-config";
-import tailwindConfig from "../../../tailwind.config.js";
-import { todo } from "../../migration.types.js";
-
 export const PAGE_SIZE = 9;
-
-export const { theme } = resolveConfig(
-  (tailwindConfig as todo) as TailwindConfig
-);
